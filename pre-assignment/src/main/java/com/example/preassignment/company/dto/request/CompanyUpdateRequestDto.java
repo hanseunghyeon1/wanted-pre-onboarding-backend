0650a31f759e20db6eb96b457a3321f216fefa72
Loopy <113760476+hanseunghyeon1@users.noreply.github.com>
2023-10-14 17:54:23 +0900

    feat: 채용 공고 수정 기능 구현

--- pre-assignment/src/main/java/com/example/preassignment/company/dto/request/CompanyUpdateRequestDto.java
+++ pre-assignment/src/main/java/com/example/preassignment/company/dto/request/CompanyUpdateRequestDto.java
@@ -1,9 +1,12 @@
 package com.example.preassignment.company.dto.request;
 
+import java.util.UUID;
 import lombok.Getter;
 
 @Getter
-public class CompanyRegistRequestDto {
+public class CompanyUpdateRequestDto {
+    private UUID companyId;
+
     private String position;
 
     private int reward;
